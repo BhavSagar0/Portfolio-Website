7d778d7e0fcffa15519a6735f8cc592b37cd4994
Bhav Sagar <bhavsagar5@gmail.com>
2023-08-10 20:25:25 +0530

    Project Completed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,33 +6,37 @@ const Contact = () => {
       <h1 className="py-4 text-4xl font-bold text-center text-[#001b5e]">
         Contact
       </h1>
-      <form action="" method="POST" encType="multipart/form-data">
+      <form action="https://getform.io/f/782f2b70-b5c2-45db-b5f3-36c16d712fff" method="POST" encType="multipart/form-data">
         <div className="grid md:grid-cols-2 gap-4 w-full py-2">
           <div className="flex flex-col">
             <label className="uppercase text-sm py-2">Name</label>
-            <input type="text" className="text" name="name" />
+            <input type="text" className="border-2 rounded-lg p-3 flex border-gray-300" name="name" />
           </div>
 
-          <div>
+          <div className="flex flex-col">
             <label className="uppercase text-sm py-2">Phone Number</label>
-            <input type="text" className="text" name="phone" />
+            <input type="text" className="border-2 rounded-lg p-3 flex border-gray-300" name="phone" />
           </div>
         </div>
 
-        <div>
+        <div className="flex flex-col py-2">
           <label className="uppercase text-sm py-2">Email</label>
-          <input type="email" name="email" />
+          <input className="border-2 rounded-lg p-3 flex border-gray-300" type="email" name="email" />
         </div>
 
-        <div>
+        <div className="flex flex-col py-2">
           <label className="uppercase text-sm py-2">Subject</label>
-          <input type="text" name="subject" id="" />
+          <input className="border-2 rounded-lg p-3 flex border-gray-300" type="text" name="subject" id="" />
         </div>
 
-        <div>
+        <div className="flex flex-col py-2">
           <label>Message</label>
-          <textarea rows='10' name="message" id="" />
+          <textarea rows='10' className="border-2 rounded-lg p-3 border-gray-300" name="message" id="" />
         </div>
+
+        <button className="bg-[#001b5e] text-gray-100 mt-4 w-full p-4 rounded-lg">
+          Send Message
+        </button>
       </form>
     </div>
   );
--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -24,7 +24,7 @@ const Sidenav = () => {
       />
       {nav && (
         <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
-          <Link
+          <Link onClick={handleNav}
             to="#main"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
@@ -32,7 +32,7 @@ const Sidenav = () => {
             <span className="pl-4">Home</span>
           </Link>
 
-          <Link
+          <Link onClick={handleNav}
             to="#work"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
@@ -40,7 +40,7 @@ const Sidenav = () => {
             <span className="pl-4">Work</span>
           </Link>
 
-          <Link
+          <Link onClick={handleNav}
             to="#projects"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
@@ -48,7 +48,7 @@ const Sidenav = () => {
             <span className="pl-4">Projects</span>
           </Link>
 
-          <Link
+          <Link onClick={handleNav}
             to="#main"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
@@ -56,7 +56,7 @@ const Sidenav = () => {
             <span className="pl-4">Resume</span>
           </Link>
 
-          <Link
+          <Link onClick={handleNav}
             to="#contact"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
